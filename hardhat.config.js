0099f39c78f7dd17c5003764d50adb9a0f6f4819
subaru-hello <octosubaru0926@gmail.com>
2022-10-15 14:52:11 +0900

    deployed TakoSeptNFT smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require('@nomiclabs/hardhat-waffle');
-// const { toHaveStyle } = require('@testing-library/jest-dom/dist/matchers');
+require('@nomiclabs/hardhat-etherscan');
 const dotenv = require('dotenv');
-// const { hexStripZeros } = require('ethers/lib/utils');
 const { task } = require('hardhat/config');
 
 dotenv.config();
@@ -17,26 +16,17 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   }
 });
 
-// toHaveStyle(
-//   'accounts',
-//   'Prints the list of accounts',
-//   async (taskArgs, hew) => {
-//     const accounts = await hexStripZeros.ethers.getSigners();
-
-//     for (const account of accoutns) {
-//       console.log(account.address);
-//     }
-//   }
-// );
 module.exports = {
   solidity: '0.8.17',
-  netwoeks: {
+  networks: {
     goerli: {
-      url: process.env.REACT_APP_GOERLI_RPC_URL,
-      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+      url: process.env.REACT_APP_ALCHEPMY_URL_WITH_API_KEY,
+      accounts: [process.env.REACT_APP_GOERLI_PRIVATE_KEY],
     },
   },
   etherscan: {
-    apikey: process.env.REACT_APP_ETHERSCAN_KEY,
+    apiKey: {
+      goerli: process.env.REACT_APP_ETHERSCAN_KEY,
+    },
   },
 };
