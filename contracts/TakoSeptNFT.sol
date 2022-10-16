febcca1cd23963c702cd2364aed771bb35303d3f
subaru-hello <octosubaru0926@gmail.com>
2022-10-16 17:31:10 +0900

    lint

--- contracts/TakoSeptNFT.sol
+++ contracts/TakoSeptNFT.sol
@@ -47,16 +47,16 @@ import "@openzeppelin/contracts/access/Ownable.sol";
          require(success, 'withdraw failed');
      }
 
-     function mint(uint256 quantity_) public payable {
-         require(isPublicMintEnabled, 'minting not enabled');
-         require(msg.value == quantity_ * mintPrice, 'wrong mint value');
-         require(totalSupply + quantity_ <= maxSupply, 'sold out');
-         require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
+    function mint(uint256 quantity_) public payable {
+      require(isPublicMintEnabled, 'minting not enabled');
+      require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+      require(totalSupply + quantity_ <= maxSupply, 'sold out');
+      require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet');
 
-         for (uint256 i = 0; i < quantity_; i++) {
-             uint256 newTokenId = totalSupply + 1;
-             totalSupply ++;
-             _safeMint(msg.sender, newTokenId);
-         }
-     }
+      for (uint256 i = 0; i < quantity_; i++) {
+        uint256 newTokenId = totalSupply + 1;
+        totalSupply ++;
+        _safeMint(msg.sender, newTokenId);
+      }
+    }
 }
